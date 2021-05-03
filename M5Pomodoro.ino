f3d3f0ccef976f7f77e54f0f2557a69f0fac429d
Naoyuki Nakadozono <nnakadozono@users.noreply.github.com>
2021-05-03 10:47:08 +0900

    refactor code. add LED blinking

--- M5Pomodoro.ino
+++ M5Pomodoro.ino
@@ -1,78 +1,87 @@
 /*
  Based on M5Stack > TFT_Clock_Digital
-
 */
 
 #include <M5StickCPlus.h>
 
 #define TFT_GREY 0x5AEB
 
-#define READY 0
-#define RUN 1
+// Status
+#define READY    0
+#define RUN      1
 #define CONTINUE 2
-#define BREAK 3
-#define SLEEP 4
-#define STOP 9
+#define BREAK    3
+#define SLEEP    4
+#define STOP     9
 
+uint8_t status = READY;
+
+// Time
 uint32_t targetTime = 0;  // for next 1 second timeout
-int8_t mm = 25, ss = 0;
-float pmm = 25;
-byte omm = 99, oss = 99;
-int internalss = 0;
-int maxinternalss = 60*5;
+int8_t mm, ss;   // minutes and seconds
+byte oldmm, oldss;  // for display
+float targetmm;           // for progress bar
 
+int internalss = 0;       // for reminder beep
+int reminderss = 5*60; // for reminder beep (senconds)
+
+int8_t npom = 0;        // number of pomodoro
+
+// Mode
+int8_t mode = 0;        // Mode. -1) Demo
+int8_t maxmode = 2; // max(mode). Check settime()
+
+void settimer(int8_t md){
+  if (md == 0) {
+    mm = 25; ss = 0;
+  } else if (md == 1) {
+    mm = 48; ss = 0;
+  } else if (md == 2) {
+    mm = 60; ss = 0;
+  } else if (md == -1) {
+    mm =  0; ss = 15;
+    //mm = 12; ss = 0;
+  }
+  targetmm = mm;
+}
+
+void setbreaktimer(int8_t md){
+  if (md == 0) {
+    mm =  5; ss = 0;
+  } else if (md == 1) {
+    mm = 12; ss = 0;
+  } else if (md == 2) {
+    mm = 15; ss = 0;
+  } else if (md == -1) {
+    mm =  0; ss = 7;
+    //mm =  3; ss = 0;
+  }
+  targetmm = mm;
+}
+
+// Display setting
+int8_t brightness = 8;
 uint32_t colorbg, colortext;
-
-int8_t pommode = 0;
-uint8_t s = READY;
-int8_t npom = 0;
-
-int xpos = 2, ypos = 34, xsecs = 0;
-int numsize = 8;
-int xpom = 16, ypom = 16, rpom = 8, xxpom;
-int xbar =  0, ybar =125, hbar = 10; // M5StickC Plus 135x240
+  // M5StickC Plus 135x240
+int xpos = 2, ypos = 34, xsecs = 0; // for numbers
+int numsize = 8;                    // font size of numbers
+int xpom = 16, ypom = 16, rpom = 8; // for pomodoro marks
+int xbar =  0, ybar =125, hbar =10; // for progress bar
 float wbar = 240.;
 
-void settimer(int8_t p){
-  if (p == 0) {
-    mm = 25; ss = 0;
-  } else if (p == 1) {
-    mm = 48; ss = 0;
-  } else if (p == 2) {
-    mm = 60; ss = 0;
-  } else if (p == -1) {
-    //mm =  0; ss = 15;
-    mm = 12; ss = 0;
-  }
-  pmm = mm;
-}
-
-void setbreaktimer(int8_t p){
-  if (p == 0) {
-    mm =  5; ss = 0;
-  } else if (p == 1) {
-    mm = 12; ss = 0;
-  } else if (p == 2) {
-    mm = 15; ss = 0;
-  } else if (p == -1) {
-    //mm =  0; ss = 7;
-    mm =  3; ss = 0;
-  }
-  pmm = mm;
-}
 
 void displaytime(){
   // Update digital time
-  if (omm != mm) { // Redraw minutes time every minute
-    omm = mm;
+  if (oldmm != mm) { // Redraw minutes time every minute
+    oldmm = mm;
     xpos = 0;
     // Draw hours and minutes
     if (mm < 10 && mm >=0) xpos += M5.Lcd.drawChar('0', xpos, ypos, numsize); // Add minutes leading zero
     xpos += M5.Lcd.drawNumber(mm, xpos, ypos, numsize);             // Draw minutes
     xsecs = xpos; // Save seconds 'x' position for later display updates
   }
-  if (oss != ss) { // Redraw seconds time every second
-    oss = ss;
+  if (oldss != ss) { // Redraw seconds time every second
+    oldss = ss;
     xpos = xsecs;
     xpos += M5.Lcd.drawChar(':', xsecs-6, ypos-8, numsize);
     //Draw seconds
@@ -82,23 +91,25 @@ void displaytime(){
 }
 
 void displaybar(){
-  if (omm != mm){
-    M5.Lcd.fillRect(xbar, ybar, (int)(wbar - (float)mm*wbar/(pmm+1.)), hbar, colortext); 
+  if (targetmm != 0){
+    if (oldmm != mm){
+      M5.Lcd.fillRect(xbar, ybar, (int)(wbar - (float)(mm+1)*wbar/targetmm), hbar, colortext); 
+    }
   }
 }
 
-void displayreset(){
-  omm = 99; oss = 99;
+void displayreset(uint8_t stts, int8_t np){
+  oldmm = 99; oldss = 99;
 
-  if (s == READY) { 
-    settimer(pommode);
+  if (stts == READY) { 
+    settimer(mode);
     colorbg = TFT_RED;
     colortext = TFT_WHITE;
-  } else if (s == CONTINUE) {
+  } else if (stts == CONTINUE) {
     colorbg = TFT_YELLOW;
     colortext = TFT_BLACK;    
-  } else if (s == BREAK) {
-    setbreaktimer(pommode);
+  } else if (stts == BREAK) {
+    setbreaktimer(mode);
     colorbg = TFT_DARKGREEN;
     colortext = TFT_WHITE;    
   }
@@ -106,20 +117,32 @@ void displayreset(){
   M5.Lcd.fillScreen(colorbg);
   M5.Lcd.setTextColor(colortext, colorbg);
   displaytime();
-  for (int i=1; i<=npom; i++){
+  int xxpom;
+  for (int i=1; i<=np; i++){
     xxpom = xpom + (i-1) * (2*rpom + 4);
     M5.Lcd.fillCircle(xxpom, ypom, rpom, colortext); 
   }
+}
 
+void beep(){
+  M5.Beep.setBeep(4000, 500);
+  M5.Beep.beep();
+  //LED
+  digitalWrite(GPIO_NUM_10, LOW);
+  delay(100);
+  digitalWrite(GPIO_NUM_10, HIGH);
 }
 
 void reminder_beep(){
-  // Adjust the time values by adding 1 second
-  internalss++;              // Advance second
-  if (internalss == maxinternalss+1) {  // Check for roll-over
-    internalss = 0;          // Reset seconds to zero
+  internalss++;                         // Advance second
+  if (internalss == reminderss+1) {  // Check for roll-over
+    internalss = 0;
+    M5.Beep.setBeep(4000, 100);
     M5.Beep.beep();
-  }
+    delay(120);
+    M5.Beep.update();  
+    M5.Beep.beep();
+  }  
 }
 
 //////////////////////////////////////////////
@@ -131,111 +154,111 @@ void setup(void) {
   M5.begin();
   M5.Lcd.setRotation(3); // 1) A is right 3) A is left
   M5.Lcd.setTextSize(1);
-  M5.Beep.setBeep(4000, 500);
-
-  M5.Axp.ScreenBreath(8);  // Display brightness
+  M5.Axp.ScreenBreath(brightness);  // Display brightness
   setCpuFrequencyMhz(10);  // 10MHz
-  
-  pommode = 0; // -1) Demo
-  npom = 0;
-  s = READY;
-  displayreset();
+
+  pinMode(GPIO_NUM_10, OUTPUT);    // LED
+  digitalWrite(GPIO_NUM_10, HIGH);
+
+  displayreset(status, npom);
 }
 
 void loop() {
   M5.update();
   M5.Beep.update();  
+
+  //** Button action ****************************************
+  // A button => Status change
   if (M5.BtnA.wasPressed()) {
-    if (s == READY || s == STOP) {
-      s = RUN;    
+    if (status == READY || status == STOP) {
+      status = RUN;    
       targetTime = millis() + 1000;
-      M5.Lcd.drawCircle(xpom + npom * (2*rpom + 4), ypom, rpom, TFT_WHITE);
-    } else if (s == RUN) {
-      s = STOP;    
-    } else if (s == CONTINUE) {
-      s = BREAK;
-      displayreset();
+      M5.Lcd.drawCircle(xpom + npom * (2*rpom + 4), ypom, rpom, colortext);
+
+    } else if (status == RUN) {
+      status = STOP;    
+
+    } else if (status == CONTINUE) {
+      status = BREAK;
+      displayreset(status, npom);
       targetTime = millis() + 1000;
-    } else if (s == BREAK) {
-      s = READY;
-      displayreset();
+
+    } else if (status == BREAK) {
+      status = READY;
+      displayreset(status, npom);
     }
   }
+
+  // B button was pressed for a long time => Reset
   if (M5.BtnB.pressedFor(1000)) {
-    s = READY;
+    status = READY;
     npom = 0;
-    displayreset();
+    displayreset(status, npom);
   }
+
+  // B button was pressed => Mode change
   if (M5.BtnB.wasPressed()) {
-    if (s == READY) {
-      if (pommode == 2) {
-        pommode = -1; // 0 to remove demo
+    if (status == READY) {
+      if (mode == maxmode) {
+        mode = -1; // 0 to remove demo
       } else {
-        pommode++;
+        mode++;
       }
     } else {
-      s = READY;
+      status = READY;
     }
-    displayreset();
+    displayreset(status, npom);
   }
+
+  // Power button was pressed => Sleep/Wake
   if (M5.Axp.GetBtnPress()==2) {
-    if(s==READY) {
-      s = SLEEP;
+    if(status == READY) {
+      status = SLEEP;
       //M5.Axp.SetSleep();
       M5.Axp.ScreenBreath(0); 
-    }else if (s==SLEEP){
-      s = READY;
-      M5.Axp.ScreenBreath(8); 
+    }else if (status == SLEEP){
+      status = READY;
+      M5.Axp.ScreenBreath(brightness); 
     }
   }
   
-  if (s == RUN || s == BREAK) {  
-    if (targetTime < millis()) {
-      // Set next update for 1 second later
-      targetTime = millis() + 1000;
+  //** Timer action ****************************************
+  if (targetTime < millis()) {
+    // Set next update for 1 second later
+    targetTime = millis() + 1000;
   
-      // Adjust the time values by adding 1 second
-      ss--;              // Advance second
-      if (ss == -1) {    // Check for roll-over
-        ss = 59;         // Reset seconds to zero
-        omm = mm;        // Save last minute time for display update
-        mm--;            // Advance minute
-      }
-      displaybar();
-      displaytime();
-      
-      if (mm == 0 && ss == 0){
-        M5.Beep.beep();
-        if (s == RUN) {
-          s = CONTINUE;
-          npom++;
-        } else if (s == BREAK) {
-          s = READY;
+    if (status == RUN || status == BREAK) {     
+        ss--;              // Advance second
+        if (ss == -1) {    // Check for roll-over
+          ss = 59;         // Reset seconds to 59
+          mm--;            // Advance minute
         }
-        displayreset();        
-      }
+        displaybar();
+        displaytime();
+        
+        if (mm == 0 && ss == 0){
+          beep();
+          if (status == RUN) {
+            status = CONTINUE;
+            npom++;
+          } else if (status == BREAK) {
+            status = READY;
+          }
+          displayreset(status, npom);
+          internalss = 0;
+        }
+
+    } else if (status == CONTINUE) {
+        ss++;              // Advance second
+        if (ss == 60) {    // Check for roll-over
+          ss = 0;          // Reset seconds to zero
+          mm++;            // Advance minute
+        }
+        displaytime(); 
+        reminder_beep();
+
+    } else if (status == READY) {
+      reminder_beep();
     }
-  } else if (s == CONTINUE) {
-    if (targetTime < millis()) {
-      // Set next update for 1 second later
-      targetTime = millis() + 1000;
-  
-      // Adjust the time values by adding 1 second
-      ss++;              // Advance second
-      if (ss == 60) {    // Check for roll-over
-        ss = 0;          // Reset seconds to zero
-        omm = mm;        // Save last minute time for display update
-        mm++;            // Advance minute
-      }
-      displaytime(); 
-      reminder_beep();
-    }    
-  } else if (s == READY) {
-    if (targetTime < millis()) {
-      // Set next update for 1 second later
-      targetTime = millis() + 1000;
-      
-      reminder_beep();
-    }    
   }
 }
